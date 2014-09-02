3114de85c9b1a2e8f2eee2ba288a9b6c48242821
Folkol <mattias4@kth.se>
2014-09-02 22:50:50 +0200

    Completed intro

--- asserts.js
+++ asserts.js
@@ -1,6 +1,7 @@
+
 assert = function(message, condition) {
     if(condition) {
-        document.write('<p class="success"></p>');
+        document.write('<p class="success">SUCCESS! - ' + message +'</p>');
     } else {
         document.write('<p class="fail">FAILED! - ' + message + '</p>');
     }
