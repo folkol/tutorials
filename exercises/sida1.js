a9587dd7c541c2b5c26dd88f096831bd5757e7cb
Folkol <mattias4@kth.se>
2014-09-03 22:48:12 +0200

    Fixed some more with QUnit

--- exercises/sida1.js
+++ exercises/sida1.js
@@ -1,5 +1,1 @@
 var foo = 'Hello world';
-
-// Test assertions - Do not change anything below this row!
-assert("Expected 'number'!", typeof foo === 'number')
-assert("Expected 'string'!", typeof bar === 'string')
---
+++ tests/sida1.js
@@ -0,0 +1,4 @@
+QUnit.test( "Check types", function( assert ) {
+  assert.equal( typeof foo, 'number', "foo had wrong type" );
+  assert.equal( typeof bar, 'boolean', "bar had wrong type" );
+});
