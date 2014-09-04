f24404335fa6a0b240f2518b1fc9b4535723f567
Mattias Johansson <mattias.johansson@atex.com>
2014-09-04 16:34:51 +0200

    Added object exercise

--- tests/sida2.js
+++ tests/sida2.js
@@ -1,3 +1,6 @@
-QUnit.test( "Assert correct types", function( assert ) {
-  assert.ok( false );
+QUnit.test( "Assert correct content structure", function( assert ) {
+    assert.equal( typeof foo, 'object', 'foo should be an Object' );
+    assert.equal( typeof foo.bar, 'object', 'foo should have a nested object called bar' );
+    assert.equal( typeof foo.bar.name, 'string' , 'foo.bar.baz should be a string' );
+    assert.equal( foo.bar.name, 'Knut', 'foo.bar should be called Knut!' );
 });
