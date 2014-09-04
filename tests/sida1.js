515482b1532471732f649272e36a19a48fcbdcf0
Mattias Johansson <mattias.johansson@atex.com>
2014-09-04 15:56:31 +0200

    Typo

--- tests/sida1.js
+++ tests/sida1.js
@@ -2,6 +2,6 @@ QUnit.test( "Assert correct types", function( assert ) {
   assert.equal( typeof foo, 'number', "Foo should be a number" );
   assert.equal( typeof bar, 'boolean', "Bar should be a boolean value" );
   assert.equal( typeof baz, 'object', "Baz should be an Object" );
-  assert.equal( typeof baz.name, 'string', "Bar should have a name" );
+  assert.equal( typeof baz.name, 'string', "Baz should have a name" );
   assert.equal( baz.name, 'kalle', "Baz should have a nice name" );
 });
