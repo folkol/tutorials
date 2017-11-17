9458331142b698fc88f8f85ba8ca2a7c3e7de6fb
Matte <mattias4@kth.se>
2017-11-17 22:27:48 +0100

    More formatting

--- golang/src/tour/packages.go
+++ golang/src/tour/packages.go
@@ -3,9 +3,12 @@ package main
 import (
     "fmt"
     "math/rand"
+    "math"
 )
 
 func main() {
     fmt.Println("My favourite number is", rand.Intn(10))
+    fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+    fmt.Println(math.Pi)
 }
 
