924262ae3836b89172cddd01610fc98c7c5da1d1
Matte <mattias4@kth.se>
2017-11-28 11:47:06 +0100

    Added aws-java-sdk-bom. Removed junit 3.

--- awssdk/src/main/java/com/folkol/tutorials/App.java
+++ awssdk/src/main/java/com/folkol/tutorials/App.java
@@ -1,13 +1,9 @@
 package com.folkol.tutorials;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
