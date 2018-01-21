99f41d9d47865f79311f95fa8acbe28c9d9e1a32
Matte <mattias4@kth.se>
2018-01-21 13:50:36 +0100

    VSCode lab

---
+++ blockchain/main.js
@@ -0,0 +1,63 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '01/01/2018', 'Genesis block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(block) {
+        // Proof of work
+        block.previousHash = this.getLatestBlock().hash;
+        block.hash = block.calculateHash();
+        this.chain.push(block);
+    }
+
+    isChainValid() {
+        // Rollback
+        for(let i = 1; i < this.chain.length; ++i) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let folkoin = new Blockchain();
+folkoin.addBlock(new Block(1, '20/01/2018', { amount: 4 }));
+folkoin.addBlock(new Block(2, '21/01/2018', { amount: 10 }));
+
+console.log(JSON.stringify(folkoin, undefined, 2));
+console.log(`Folkoin is valid: ${folkoin.isChainValid()}`);
+folkoin.chain[1].data.amount = 666;
+console.log(`Folkoin is valid: ${folkoin.isChainValid()}`);
