7795fb0e1dd31e0ba43f44bafa4fe69887d932b0
Matte <mattias4@kth.se>
2018-03-31 15:02:26 +0200

    Example of a module

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,14 @@
-function sayHello(name) {
-	console.log(`Hello ${name}!`);
-}
+// Main module
 
-sayHello('Folkol');
+// function sayHello(name) {
+// 	console.log(`Hello ${name}!`);  // or global.console.log...
+// }
+
+// sayHello('Folkol');
+
+console.log(module);  // NOT global.module
+
+const logger = require('./logger');
+console.log(logger);
+logger.log('wut');
+
