dbb74ba98c03fcaea5850b2b8286fcc097d541cd
Matte <mattias4@kth.se>
2018-03-31 15:24:00 +0200

    More modules

--- nodejs/app.js
+++ nodejs/app.js
@@ -12,8 +12,23 @@
 // console.log(logger);
 // logger.log('wut');
 
-const path = require('path');
-const pathObj = path.parse(__filename);
+// const path = require('path');
+// const pathObj = path.parse(__filename);
 
-console.log(pathObj);
+// console.log(pathObj);
 
+// const os = require('os');
+// console.log(Object.keys(os));
+
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
+
+// console.log(`Total memory: ${totalMemory}, Free memory: ${freeMemory}`);
+
+const fs = require('fs');
+
+// const files = fs.readdirSync('.');
+// console.log(files);
+
+const files = fs.readdir('.', (err, files) => console.log('Result:', err || files));
+// console.log(files);
