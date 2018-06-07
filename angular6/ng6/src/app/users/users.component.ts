bef33ba9e9a80d0ddfcb20a069623c77b4713b50
Matte <mattias4@kth.se>
2018-06-07 23:50:03 +0200

    Added mock data

--- angular6/ng6/src/app/users/users.component.ts
+++ angular6/ng6/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users$: Object;
 
-  ngOnInit() {
+  constructor(private data: DataService) { }
+
+  ngOnInit() {  // life cycle hooks
+  	this.data.getUsers().subscribe(data => this.users$ = data);
   }
 
 }
