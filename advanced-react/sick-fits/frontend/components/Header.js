d1746dd02df1bab9832c4b7edc69ac14d53df2dd
Matte <mattias.johansson@ivbar.com>
2019-01-16 15:33:13 +0100

    Added some components

---
+++ advanced-react/sick-fits/frontend/components/Header.js
@@ -0,0 +1,11 @@
+export default () => (
+	<div>
+		<div classname="bar">
+			<a href="">Sick Fits</a>
+		</div>
+		<div classname="sub-bar">
+			<p>Search</p>
+		</div>
+		<div>Cart</div>
+	</div>
+)
